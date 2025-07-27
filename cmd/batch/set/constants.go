901e473aab5ae8ad89fbc7586bf0a634c3133020
tamaco489 <tamaco489@gmail.com>
2025-07-27 15:19:56 +0900

    fix constants

--- cmd/batch/set/constants.go
+++ cmd/batch/set/constants.go
@@ -9,13 +9,17 @@ const (
 // User ID constants
 const (
 	UserIDStart = 10000001
-	UserIDEnd   = 10001000
+	UserIDEnd   = 10100000
+	UserIDCount = 1000 // ランダムに選択する数
+	// UserIDCount = 3 // ランダムに選択する数 (少量のデータで確認する時用)
 )
 
 // Item ID constants
 const (
-	ItemIDStart = 30000001
-	ItemIDEnd   = 30001000
+	ItemIDStart    = 30000001
+	ItemIDEnd      = 30100000
+	ItemIDMinCount = 100 // 最小選択数
+	ItemIDMaxCount = 500 // 最大選択数
 )
 
 // Redis key format constants
